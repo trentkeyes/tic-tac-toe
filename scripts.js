ebd0d1e628d1631f070caa7e3e22ebd921e8baf6
trentkeyes <tkeyes@gmail.com>
2022-06-02 10:57:57 -0500

    Game can end in tie, stops allowing play after game end

--- scripts.js
+++ scripts.js
@@ -1,71 +1,93 @@
-const Player = name => {
-    let myTurn = false;
-    return { name, myTurn };
-  }
-let playerX = Player('X');
-let playerO = Player('O');
+const Player = (name) => {
+  return { name };
+};
+let playerX = Player("X");
+let playerO = Player("O");
 
 const game = (() => {
-    const board = [null, null, null, null, null, null, null, null, null];
-    const playerChoice = function(choice, cell) {
-      if (!board[cell]) {
-        board[cell] = choice;
-      } 
-    }
-    let currentPlayer = playerX;
-    let turnCount = 0;
-
-    const endGame = function() {
-      if (turnCount >= 9) {
-        console.log('tie game');
-      }
-      if (board[0] === board[1] && board[0] === board[2] && board[1] === board[2] && board[0] !== null 
-         || board[3] === board[4] && board[3] === board[5] && board[4] === board[5] && board[3] !== null 
-         || board[6] === board[7] && board[6] === board[8] && board[7] === board[8] && board[6] !== null 
-         || board[0] === board[3] && board[0] === board[6] && board[3] === board[6] && board[0] !== null 
-         || board[1] === board[4] && board[1] === board[7] && board[4] === board[7] && board[1] !== null 
-         || board[2] === board[5] && board[2] === board[8] && board[5] === board[8] && board[2] !== null 
-         || board[0] === board[4] && board[0] === board[8] && board[4] === board[8] && board[0] !== null 
-         || board[2] === board[4] && board[2] === board[6] && board[4] === board[6] && board[2] !== null ) {
+  const board = [null, null, null, null, null, null, null, null, null];
+  let currentPlayer = playerX;
+  let turnCount = 0;
+  let gameOver = false;
+  let winner;
+  const playTurn = function () {
+    if (gameOver === false) {
+      if (turnCount >= 8) {
+        console.log("tie game");
+        gameOver = true;
+      } else if (
+        (board[0] === board[1] &&
+          board[0] === board[2] &&
+          board[1] === board[2] &&
+          board[0] !== null) ||
+        (board[3] === board[4] &&
+          board[3] === board[5] &&
+          board[4] === board[5] &&
+          board[3] !== null) ||
+        (board[6] === board[7] &&
+          board[6] === board[8] &&
+          board[7] === board[8] &&
+          board[6] !== null) ||
+        (board[0] === board[3] &&
+          board[0] === board[6] &&
+          board[3] === board[6] &&
+          board[0] !== null) ||
+        (board[1] === board[4] &&
+          board[1] === board[7] &&
+          board[4] === board[7] &&
+          board[1] !== null) ||
+        (board[2] === board[5] &&
+          board[2] === board[8] &&
+          board[5] === board[8] &&
+          board[2] !== null) ||
+        (board[0] === board[4] &&
+          board[0] === board[8] &&
+          board[4] === board[8] &&
+          board[0] !== null) ||
+        (board[2] === board[4] &&
+          board[2] === board[6] &&
+          board[4] === board[6] &&
+          board[2] !== null)
+      ) {
         console.log(`Player ${currentPlayer.name} is the winner!`);
+        winner = currentPlayer.name;
+        gameOver = true;
       }
+      turnCount++;
     }
-    return {
-      board,
-      playerChoice,
-      endGame,
-      currentPlayer,
-      turnCount
-    };
-  })();
+  };
+  return {
+    board,
+    playTurn,
+    currentPlayer,
+    turnCount,
+    gameOver,
+  };
+})();
 
 const gameBoard = (() => {
-    const cells = Array.from(document.querySelectorAll('.tttCell'));
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].addEventListener('click', event => {
-        cells[i].id = i;
-        addChoice(cells[i]);
-      });
-    }
-    const addChoice = cell => {
+  const cells = Array.from(document.querySelectorAll(".tttCell"));
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", (event) => {
+      cells[i].id = i;
+      addChoice(cells[i]);
+    });
+  }
+  const addChoice = (cell) => {
+    if (game.gameOver === false) {
       if (game.board[cell.id] === null) {
         cell.textContent = game.currentPlayer.name;
         game.board[cell.id] = game.currentPlayer.name;
-        game.turnCount++;
         if (game.currentPlayer === playerX) {
-        game.currentPlayer = playerO;
+          game.currentPlayer = playerO;
         } else {
-        game.currentPlayer = playerX;
+          game.currentPlayer = playerX;
         }
-      game.endGame();
+        game.playTurn();
       }
-     
-
     }
-  })();
-
-
-
+  };
+})();
 
 // const Player = (name) => {
 //     const sayHi = () => console.log(`I am player ${name}`);
@@ -92,51 +114,49 @@ const gameBoard = (() => {
 //       } else if (player1.XorO === 'o') {
 //         XorO = 'x';
 //       }
-    
+
 //     }
-    // be able to make choice or not
-    // const setName = name => {
-    //   console.log(`I'm ${name}`);
-    //   const playerX = Player(name);
-    //   playerX.turnOrder = 'first';
-    //   return playerX;
-    // }
-    // x.addEventListener('click', event => {
-    //     setName('x');
-    //     console.log('this is an event');
-    //   });
-    
-  //   return { name, sayHi, myTurn, XorO };
-  // }
-  
-  // let player1 = Player(1);
-  // let player2 = Player(2);
+// be able to make choice or not
+// const setName = name => {
+//   console.log(`I'm ${name}`);
+//   const playerX = Player(name);
+//   playerX.turnOrder = 'first';
+//   return playerX;
+// }
+// x.addEventListener('click', event => {
+//     setName('x');
+//     console.log('this is an event');
+//   });
 
- // const playerO = Player('o');
- // console.log(playerX.set());
+//   return { name, sayHi, myTurn, XorO };
+// }
 
-  // playerfactory should tell me who the current player is
-  // each player is x or o. 'game' can decide "whose turn is it?"
-  //module returns the functions we want publicly available(like add, sub)
-  
-  // gameBoard.playerChoice('o', 2);
-  // gameBoard.playerChoice('x', 0);
-  // gameBoard.playerChoice('o', 1);
-  // gameBoard.playerChoice('x', 3); 
-  // gameBoard.playerChoice('o', 4);
-  // gameBoard.playerChoice('x', 5);
-  // gameBoard.playerChoice('o', 7);
-  // gameBoard.playerChoice('o', 6);
-  // gameBoard.playerChoice('x', 8);
-  // console.log(gameBoard.board);
-  // console.log(gameBoard.endGame());
-  
-  // create player objects, first create generic factory functions
-  // displayController module which accesses gameboard and checks array and displays changes, look into video where he had his personChange function
-  // can check if the spot in the array is not null, to know if a player has already chosen that spot
-  
-  // Each little piece of functionality should be able to fit in the game, player or gameboard objects
-  
-  // make logic for a tie, board is full and no winner
-  
-  
+// let player1 = Player(1);
+// let player2 = Player(2);
+
+// const playerO = Player('o');
+// console.log(playerX.set());
+
+// playerfactory should tell me who the current player is
+// each player is x or o. 'game' can decide "whose turn is it?"
+//module returns the functions we want publicly available(like add, sub)
+
+// gameBoard.playerChoice('o', 2);
+// gameBoard.playerChoice('x', 0);
+// gameBoard.playerChoice('o', 1);
+// gameBoard.playerChoice('x', 3);
+// gameBoard.playerChoice('o', 4);
+// gameBoard.playerChoice('x', 5);
+// gameBoard.playerChoice('o', 7);
+// gameBoard.playerChoice('o', 6);
+// gameBoard.playerChoice('x', 8);
+// console.log(gameBoard.board);
+// console.log(gameBoard.endGame());
+
+// create player objects, first create generic factory functions
+// displayController module which accesses gameboard and checks array and displays changes, look into video where he had his personChange function
+// can check if the spot in the array is not null, to know if a player has already chosen that spot
+
+// Each little piece of functionality should be able to fit in the game, player or gameboard objects
+
+// make logic for a tie, board is full and no winner
